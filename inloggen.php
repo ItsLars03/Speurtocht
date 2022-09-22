5523e56b7c1fc9f83c3cc020d8fed87790f43223
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2022-09-22 14:35:53 +0200

    Registreren & Login frontend

--- inloggen.php
+++ inloggen.php
@@ -2,17 +2,18 @@
 <?php include('header.php'); ?>
 <div class="content">
     <div class="titleBox">
-        <h2 class="pageTitle"> Registreren </h2>
+        <h2 class="pageTitle"> Aanmelden </h2>
     </div>
 <?php
 
 ?>
- <form action="/backend-register.php">
+ <form action="/backend-login.php">
   <input class="login-register-form" type="text" id="email" placeholder="E-mailadres"><br><br>
   <input class="login-register-form" type="password" id="password" placeholder="Wachtwoord"><br><br>
-  <input class="login-register-btn" type="submit" value="REGISTREREN">
+  <input class="login-register-btn" type="submit" value="AANMELDEN">
 </form> 
 </div>
 
 </body>
 </html>
+
