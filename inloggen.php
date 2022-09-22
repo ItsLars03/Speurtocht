d0322cff943b25a8fd12dfd654451eb35fa87c1c
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2022-09-22 15:03:00 +0200

    Optie om in te loggen of re registreren
    Updated

--- inloggen.php
+++ inloggen.php
@@ -11,8 +11,9 @@
   <input class="login-register-form" type="text" id="email" placeholder="E-mailadres"><br><br>
   <input class="login-register-form" type="password" id="password" placeholder="Wachtwoord"><br><br>
   <input class="login-register-btn" type="submit" value="AANMELDEN">
+  <h4> </h4> 
+  <a class="elseRegister" href="/register">Nog geen account account? Maak er een</a>
 </form> 
-<b>This text is bold</b> 
 </div>
 
 </body>
