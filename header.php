1db3821ad4b1f23b7f569fd9b261a68f85acd562
JB1 <74594035+JBG12@users.noreply.github.com>
2022-09-22 13:05:10 +0200

    Eerste bestanden

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="speurtocht.css">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Black Han Sans' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Source Sans Pro' rel='stylesheet'>
+    <title>Speurtocht</title>
+</head>
+<body>
+    <div class="headerBox">
+        <h1 class="headerTitle"> Speurtocht </h1>
+        
+    </div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+<div class="content">
+    <div class="titleBox">
+        <h2 class="pageTitle"> Aanmelden </h2>
+    </div>
+<?php
+
+
+
+?>
+
+</div>
+
+</body>
+</html>
