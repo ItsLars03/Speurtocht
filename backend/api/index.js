58210cab82e53426cefa1dc667a8b4d1e7ca25e3
Thiimo <thimosietsma@gmail.com>
2022-09-27 14:16:46 +0200

    login, user & scavengerHunt routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
-import login from './login/index.js'
+import login from './user/login/index.js'
 import scavengerhunt from './scavengerHunt/index.js'
+import users from './user/users.js'
 
 const router = Router()
 
-router.get("/", (req, res) => {
-    res.send({ message: "hi" })
-})
+// router.get("/", (req, res) => {
+//     res.send({ message: "hi" })
+// })
 
-router.post("/test", (req, res) => {
-    console.log("BODY!", req.body)
-    res.status(200).send({ message: "success!" })
-})
+// router.post("/test", (req, res) => {
+//     console.log("BODY!", req.body)
+//     res.status(200).send({ message: "success!" })
+// })
 
-router.use("/login", login)
+router.use("/users/login", login)
+router.use("/users", users)
 router.use("/scavengerhunt", scavengerhunt)
 
 export default router
