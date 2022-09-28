ad1cd0af34f51b3865eed77ded02d6ea865dd757
Thiimo <thimosietsma@gmail.com>
2022-09-28 13:56:15 +0200

    db change

--- backend/api/scavengerHunt/index.js
+++ backend/api/scavengerHunt/index.js
@@ -26,53 +26,106 @@ router.get("/", async (req, res) => {
 
 //TODO -- just dummy data now.
 router.post("/", async (req, res) => {
-    const data = await prisma.scavengerHunt.create({
-        data: {
-            User: {
-                connect: {
-                    email: "email"
-                }
-            },
-            name: "test",
-            status: "closed",
-            questions: {
-                createMany: {
-                    data: [
-                        {
-                            question: "question 1",
-                            type: "text",
-                            // answers: {
-                            //     data: null
-                            // }
-                        },
-                        {
-                            question: "question 2",
-                            type: "photo",
-                            // answers: {
-                            //     data: null
-                            // }
-                        }
-                    ]
-                }
-            },
-            players: {
-                createMany: {
-                    data: [
-                        {
-                            name: "player1",
-                            email: "player1@gmail.com"
-                        },
-                        {
-                            name: "player2",
-                            email: "player2@gmail.com"
-                        }
-                    ]
-                }
-            }
-        }
-    })
+    /**
+     * What you need:
+     *  - user
+     *  - name
+     *  - questions?
+     *  - status - default -> closed
+     *  - players - default -> empty[]
+     */
 
-    res.status(200).json({ ...data })
+    const {
+        email,
+        name,
+        questions = [],
+        status = "closed",
+        players = []
+    } = req.body
+
+
+    if (!email || !name) {
+        res.status(400).json({
+            success: false,
+            message: "email and name are required fields."
+        })
+        return
+    }
+
+    try {
+        const data = await prisma.scavengerHunt.create({
+            data: {
+                User: {
+                    connect: {
+                        email
+                    }
+                },
+                name,
+                status,
+                questions,
+                players
+            }
+        })
+
+        res.status(200).json({
+            success: true,
+            data,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error."
+        })
+    }
+
+
+    // const data = await prisma.scavengerHunt.create({
+    //     data: {
+    //         User: {
+    //             connect: {
+    //                 email: "email"
+    //             }
+    //         },
+    //         name: "test",
+    //         status: "closed",
+    //         questions: {
+    //             createMany: {
+    //                 data: [
+    //                     {
+    //                         question: "question 1",
+    //                         type: "text",
+    //                         // answers: {
+    //                         //     data: null
+    //                         // }
+    //                     },
+    //                     {
+    //                         question: "question 2",
+    //                         type: "photo",
+    //                         // answers: {
+    //                         //     data: null
+    //                         // }
+    //                     }
+    //                 ]
+    //             }
+    //         },
+    //         players: {
+    //             createMany: {
+    //                 data: [
+    //                     {
+    //                         name: "player1",
+    //                         email: "player1@gmail.com"
+    //                     },
+    //                     {
+    //                         name: "player2",
+    //                         email: "player2@gmail.com"
+    //                     }
+    //                 ]
+    //             }
+    //         }
+    //     }
+    // })
+
+    // res.status(200).json({ ...data })
 })
 
 router.delete("/", async (req, res) => {
