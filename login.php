0b2563d7ae0eaa3c8c92abca5d63add0c2fad212
Thiimo <thimosietsma@gmail.com>
2022-10-04 09:15:17 +0200

    fixes

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
         <input class="login-register-form" name="password" type="password" id="password" placeholder="Wachtwoord"><br><br>
         <input class="login-register-btn" name="submit" type="submit" value="AANMELDEN">
         <h4> </h4>
-        <a class="elseRegister" href="/register">Nog geen account account? Maak er een</a>
+        <a class="elseRegister" href="/register.php">Nog geen account account? Maak er een</a>
     </form>
 </div>
 
