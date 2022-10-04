45da9534734b22845f326de1d4d4f676da3a029c
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2022-10-04 14:12:24 +0200

    Fix

--- results.php
+++ results.php
@@ -1,117 +1,98 @@
 <html>
-<?php include ('header.php'); ?>
-<div class="content">
-    <div class="titleBox">
-        <h2 class="pageTitle"> Scoreboard </h2>
-    </div>
-
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "resulta";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-}
-
-?>
-
-<html>
-<div id="wrapper">
-    <div id="sc1">
-<?php
-// 1th winner
-$query = "SELECT teamname AS tn_1th FROM results GROUP by points ORDER BY  points DESC LIMIT 0 , 1";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 1th">' . $row['tn_1th'] . '</div>';
-}
-
-// 2th winner
-$query = "SELECT teamname AS tn_2th FROM results GROUP by points ORDER BY  points DESC LIMIT 1 , 1";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 2th">' . $row['tn_2th'] . '</div>';
-}
-
-// 3th winner
-$query = "SELECT teamname AS tn_3th FROM results GROUP by points ORDER BY  points DESC LIMIT 2 , 1";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 3th">' . $row['tn_3th'] . '</div>';
-}
-
-// losers
-$query = "SELECT teamname AS tn_losers FROM results GROUP by points ORDER BY  points DESC LIMIT 3 , 10";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard losers">' . $row['tn_losers'] . '</div>';
-}
-?>
-</div>
-
-<div id="sc2">
-
-<?php
-// 1th winner
-$query = "SELECT teamname , MAX(points) AS 1th FROM `results`";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 1th">' . $row['1th'] . '</div>';
-}
-
-// 2th winner
-$query = "SELECT teamname , points AS 2th FROM results GROUP by points ORDER BY  points DESC LIMIT 1 , 1";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 2th">' . $row['2th'] . '</div>';
-}
-
-// 3th winner
-$query = "SELECT teamname , points AS 3th FROM results GROUP by points ORDER BY  points DESC LIMIT 2 , 1";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard 3th">' . $row['3th'] . '</div>';
-}
-
-// losers
-$query = "SELECT teamname , points AS losers FROM results GROUP by points ORDER BY  points DESC LIMIT 3 , 10";
-$query_result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_assoc($query_result))
-{
-    echo '<div class="scoreboard losers">' . $row['losers'] . '</div>';
-}
-
-?>
-</div>
-</div>
-</html>
-<?php
-
-// $sql = "SELECT * FROM results ORDER BY points DESC limit 10";
-// $result = $conn->query($sql);
-
-
-$conn->close();
-?> 
-<html>
+   <?php include "header.php"; ?>
+   <?php include "utils/connection.php" ?>
+   <div class="content">
+   <div class="titleBox">
+      <h2 class="pageTitle"> Eindstand </h2>
+   </div>
+   <?php
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+      ?>
+   <html>
+      <div id="wrapper">
+         <div id="sc1">
+            <?php
+               // 1th winner
+               $query =
+                   "SELECT teamname AS tn_1th FROM results GROUP by points ORDER BY  points DESC LIMIT 0 , 1";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 1th">' . $row["tn_1th"] . "</div>";
+               }
+               
+               // 2th winner
+               $query =
+                   "SELECT teamname AS tn_2th FROM results GROUP by points ORDER BY  points DESC LIMIT 1 , 1";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 2th">' . $row["tn_2th"] . "</div>";
+               }
+               
+               // 3th winner
+               $query =
+                   "SELECT teamname AS tn_3th FROM results GROUP by points ORDER BY  points DESC LIMIT 2 , 1";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 3th">' . $row["tn_3th"] . "</div>";
+               }
+               
+               // losers
+               $query =
+                   "SELECT teamname AS tn_losers FROM results GROUP by points ORDER BY  points DESC LIMIT 3 , 10";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard losers">' . $row["tn_losers"] . "</div>";
+               }
+               ?>
+         </div>
+         <div id="sc2">
+            <?php
+               // 1th winner
+               $query = "SELECT teamname , MAX(points) AS 1th FROM `results`";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 1th">' . $row["1th"] . "</div>";
+               }
+               
+               // 2th winner
+               $query =
+                   "SELECT teamname , points AS 2th FROM results GROUP by points ORDER BY  points DESC LIMIT 1 , 1";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 2th">' . $row["2th"] . "</div>";
+               }
+               
+               // 3th winner
+               $query =
+                   "SELECT teamname , points AS 3th FROM results GROUP by points ORDER BY  points DESC LIMIT 2 , 1";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard 3th">' . $row["3th"] . "</div>";
+               }
+               
+               // losers
+               $query =
+                   "SELECT teamname , points AS losers FROM results GROUP by points ORDER BY  points DESC LIMIT 3 , 10";
+               $query_result = mysqli_query($conn, $query);
+               
+               while ($row = mysqli_fetch_assoc($query_result)) {
+                   echo '<div class="scoreboard losers">' . $row["losers"] . "</div>";
+               }
+               ?>
+         </div>
+      </div>
+   </html>
+   <?php // $sql = "SELECT * FROM results ORDER BY points DESC limit 10";
+      // $result = $conn->query($sql);
+      
+      $conn->close(); ?> 
+   <html>
--- utils/connection.php
+++ utils/connection.php
@@ -1,8 +1,12 @@
 <?php
 
-$dbServer = "localhost";
-$dbUsername = "root";
-$dbPassword = "password";
-$dbName = "db";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "resulta";
 
-$con = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName) or trigger_error("Failed to connect to the database.");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname) or trigger_error("Kan niet verbinden met de database, neem contact op met de beheerder.");
+// Check connection
+
+// $con = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName) or trigger_error("Failed to connect to the database.");
