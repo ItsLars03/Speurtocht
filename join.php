42e08c53becd32f863f52b323f0499a8b3e9ffaf
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2022-10-05 08:54:19 +0200

    Frontend join

--- join.php
+++ join.php
@@ -1,43 +1,19 @@
-<?php
-include('./header.php');
-include('./utils/api.php');
-
-
-//check valid uri
-// - if not redirect to root.
-if (!isset($_GET['id'])) {
-    echo "no id?";
-    // header("Location: /");
-    return;
-}
-
-$response = API::get('/mail/' . $_GET['id'], array());
-
-//invalid id
-//TODO: Maybe make an error message instead of redirecting.
-if (!isset($response) || !isset($response->success) || !$response->success) {
-    if (isset($response->message)) echo $response->message;
-    // else header("Location: /");
-    var_dump($response);
-    return;
-}
-
-$email = $response->data->email;
-$scavengerHuntId = $response->data->scavengerHuntId;
-?>
-
+<html>
+<?php include('header.php'); ?>
 <div class="content">
     <div class="titleBox">
-        <h2 class="pageTitle"> Login </h2>
+        <h2 class="pageTitle"> Join </h2>
     </div>
+    <?php
 
+    ?>
     <form action="/server/join.php">
-        <input class="login-register-form" name="name" type="text" id="name" placeholder="Naam">
-        <br />
-        <input class="login-register-form" type="hidden" name="email" value="<?php echo $email ?>">
-        <input class="login-register-form" type="hidden" name="scavengerhuntid" value="<?php echo $scavengerHuntId ?>">
-        <br />
-        <input class="login-register-btn" type="submit" value="login">
+        <input class="login-register-form" name="join" type="text" id="join" placeholder="Invite code"><br><br>
+        <input class="login-register-btn" name="submit" type="submit" value="DEELNEMEN">
+        <h4> </h4>
     </form>
-
 </div>
+
+</body>
+
+</html>
