24b088118b8c91facf4bce88add70858fa2002e4
Thiimo <thimosietsma@gmail.com>
2022-10-06 19:30:19 +0200

    fixes

--- questions.php
+++ questions.php
@@ -5,11 +5,31 @@ include("./utils/api.php");
 //get random question
 
 
+//Random vraag ophalen.
 $getQuestionRes = API::get("/scavengerhunt/questions/random/" . $_COOKIE['player-id'], array());
-
 if (!isset($getQuestionRes) || isset($getQuestionRes->success) || !$getQuestionRes->success) {
     //error.
     return;
 }
 
+$question = $getQuestionRes->data;
+
 var_dump($getQuestionRes);
+
+
+//Text vraag beantwoorden
+$answerRes = API::post("/scavengerhunt/answers/", [
+    "questionId" => $question->questionId,
+    "playerId" => $_COOKIE['player-id'],
+    "answer" => "answer here.",
+]);
+
+if (!isset($answerRes) || isset($answerRes->success) || !$answerRes->success) {
+    //error.
+    return;
+}
+
+$answer = $answerRes->data;
+
+//Foto vraag beantwoorden
+$photoAnswerRes = API::postFile("/scavengerhunt/answer/photo", [], "", "", "");
